7f77f1ecda0fe32fe7e397aafafd7964711071a3
LaughingLeader <laughingleader@gmail.com>
2023-08-19 21:32:07 -0500

    Add a script to dump all effects to a tsv

---
+++ scripts/extract_effects.py
@@ -0,0 +1,80 @@
+import os
+from pathlib import Path
+import lxml.etree as ET
+from numpy import int64
+import argparse
+
+from dataclasses import dataclass
+
+import common
+script_name = Path(__file__).stem
+common.clear_log(script_name)
+
+script_dir = Path(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(script_dir)
+
+@dataclass
+class Effect:
+    name:str
+    uuid:str
+    filepath:Path
+
+    def __str__(self) -> str:
+        return f"{self.name}\t{self.uuid}\t{self.filepath}"
+
+default_output_path = Path(script_dir.joinpath("output\\effects\\"))
+default_divine_path = Path(os.environ.get("LSLIB_PATH", None))
+
+## cli args here
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--divine", type=Path, default=default_divine_path, help="The path to divine.exe.")
+parser.add_argument("-o", "--output", type=Path, default=default_output_path, help="The output directory.")
+parser.add_argument("-e", "--effects", type=Path, default=Path("G:/Modding/BG3/_Extracted/Public/Shared/Content/Assets/Effects"), help="The effects directory.")
+
+parser.description = "Extract effect data from a path."
+new_line = "\n    "
+parser.usage = f"""
+Example usage:
+python extract_osiris.py -d "C:\lslib\divine.exe" -o "C:\Modding\BG3\OsirisInfo" -f "%LOCALAPPDATA%\Larian Studios\Baldur's Gate 3\PlayerProfiles\Public\Savegames\Story\Tav-24112318724__Quicksave1\Quicksave1.lsv"
+"""
+
+args = parser.parse_args()
+
+lslib_dll:Path = args.divine.is_dir() and args.divine.joinpath("LSLib.dll") or args.divine.parent.joinpath("LSLib.dll")
+output_dir:Path = args.output
+output_dir.mkdir(exist_ok=True, parents=True)
+data_dir:Path = args.effects
+
+effects:dict[str,Effect] = {}
+
+for f_path in data_dir.rglob("*.lsx"):
+    with f_path.open("r", encoding="utf-8") as f:
+        meta_xml:ET._Element = ET.XML(f.read(), None)
+        #for node in meta_xml.iterfind("./region/node/children/node[@id='Resource']/attribute", None):
+        for node in meta_xml.iterfind("./region/node/children/node[@id='Resource']", None):
+            name = ""
+            uuid = ""
+            source_file:Path = None
+            node:ET._Element
+            is_effect = False
+            for attr in node.iterchildren("attribute"):
+                value = attr.get("value", "")
+                match(attr.get("id", "")):
+                    case "EffectName":
+                        is_effect = True
+                        name = value
+                    case "SourceFile":
+                        source_file = Path(value)
+                    case "ID":
+                        uuid = value
+                if is_effect and source_file != None and uuid != "":
+                    break
+            if is_effect and name != "" and uuid != "":
+                effect = Effect(name, uuid, source_file)
+                effects[name] = effect
+                
+output_file = output_dir.joinpath("Effects.tsv")
+sorted_effects = [str(effects[k]) for k in sorted(effects.keys())]
+output_txt = "Name\tUUID\tFilePath\n"
+output_file.write_text(output_txt + "\n".join(sorted_effects), encoding="utf-8")
+print("Done")
